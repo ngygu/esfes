5d263b7fd1691b5d402fd3b498af89becd9189a8
ngygu <124871708+ngygu@users.noreply.github.com>
2023-03-04 06:36:53 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-uuidone=0e8cc52e-caf5-44aa-90c1-697e49fd8887
-uuidtwo=56a697d8-3fcd-4cf0-859c-9ab1b5df58df
-uuidthree=fbcfc0ba-76ce-4fb6-a61a-ed239b70b802
-uuidfour=50da9c1f-bf7a-47fa-b933-f7ad52161d41
-uuidfive=281204fe-1c43-4d9d-8727-b2ba90fc9863
-mypath=/file-myfile
+uuidone=bdbe10ff-366d-44e4-b2ec-82eec40bb185
+uuidtwo=66da3187-8eb9-4627-ad3b-ebf9f63edaa4
+uuidthree=eafe75a8-6445-4bc6-b6cd-669c6cab098b
+uuidfour=073c84e4-bb3d-459f-ba19-db3500ec3c13
+uuidfive=6d73813d-6b86-4d88-8b81-963e683d37ea
+mypath=/helloword-test
 myport=8080
 
 
